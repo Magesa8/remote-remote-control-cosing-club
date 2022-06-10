c8adf9bd5a5955225fc840a00a7c4392e7d3bd3d
Magesa8 <allie@autistry.com>
2022-06-09 23:08:10 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,10 @@
+input.onButtonPressed(Button.A, function () {
+    radio.sendString("left")
+})
+input.onButtonPressed(Button.AB, function () {
+    radio.sendString("startstop")
+})
+input.onButtonPressed(Button.B, function () {
+    radio.sendString("right")
+})
+radio.setGroup(19)
